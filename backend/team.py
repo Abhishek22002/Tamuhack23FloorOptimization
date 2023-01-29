68c8e2ce2104ef4fca1ca42351055fb4298b585f
Boting Lu <botinglu@utexas.edu>
2023-01-28 20:05:00 -0600

    Changed snake to camel case

--- backend/team.py
+++ backend/team.py
@@ -2,9 +2,9 @@ from typing import List
 
 class team:
 
-    def __init__(self, name:str, strength:int, preferred:List[int], tolerated:List[int], no_way:List[int]) -> None:
+    def __init__(self, name:str, strength:int, preferred:List[int], tolerated:List[int], noWay:List[int]) -> None:
         self.name = name
         self.strength = strength
         self.preferred = preferred
         self.tolerated = tolerated
-        self.no_way = no_way
+        self.noWay = noWay
