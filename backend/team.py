62a04fca297df659d8cbeb6864e5b9b67f66836c
a2zaustin <a2zaustin@gmail.com>
2023-01-28 20:03:10 -0600

    floor class

--- backend/team.py
+++ backend/team.py
@@ -1,6 +1,12 @@
 from typing import List
 
-class team:
+class floor():
+
+    def __init__(self, name:str, max_capacity:int) -> None:
+        self.name = name
+        self.max_capacity = max_capacity
+
+class team():
 
     def __init__(self, name:str, strength:int, preferred:List[int], tolerated:List[int], no_way:List[int]) -> None:
         self.name = name
@@ -8,3 +14,9 @@ class team:
         self.preferred = preferred
         self.tolerated = tolerated
         self.no_way = no_way
+
+class Algo():
+
+    def __init__(self, teamList:List[team], floorList:List[floor]) -> None:
+        print("test")
+
